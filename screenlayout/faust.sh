64f954812b7dd369cabe39b7f6240c57f3aad950
Henri Burau <henri.burau@web.de>
2019-05-18 12:02:31 +0200

    Add ros_make and faust screenlayout

--- screenlayout/faust.sh
+++ screenlayout/faust.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI1 --mode 1920x1200 --pos 1366x0 --rotate normal --output LVDS1 --primary --mode 1366x768 --pos 0x760 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --off
+xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --output LVDS1 --primary --mode 1366x768 --pos 0x520 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --off
