fca7cd5293ea3a82c0937ee5502d653c612eb8f1
Henri Burau <henri.burau@web.de>
2019-11-20 10:26:43 +0100

    Add diverse

---
+++ dunst_test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+notify-send -u low "Low" "This is a low notification"
+notify-send -u normal "Normal" "This is a normal notification"
+notify-send -u critical "Critical" "This is a critical notification"
+notify-send -u normal "New mail" "This is a mail notification"
--- screenlayout/viamint.sh
+++ screenlayout/viamint.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-xrandr  --newmode "1920x1080_30.00"   79.75  1920 1976 2168 2416  1080 1083 1088 1102 -hsync +vsync
-
-xrandr --verbose --addmode HDMI1 "1920x1080_30.00"
-
-xrandr --output HDMI1 --mode "1920x1080_30.00" --pos 0x0 --rotate normal --output LVDS1 --primary --mode 1366x768 --pos 1920x544 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --off
-
+xrandr --output DP-3 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off
